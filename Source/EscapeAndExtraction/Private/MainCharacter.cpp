ac00d20ca9320a6d06df4462792bf8c6e2b42638
VadyaS <notfoundmyname@gmail.com>
2026-08-16 19:31:08 +0300

    BaseComponents
    Added Stamina and Health

--- Source/EscapeAndExtraction/Private/MainCharacter.cpp
+++ Source/EscapeAndExtraction/Private/MainCharacter.cpp
@@ -3,22 +3,29 @@
 
 #include "MainCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/CapsuleComponent.h"
 #include "Camera/CameraComponent.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "InputActionValue.h"
 #include "PlayerControllerMain.h"
+#include "StaminaComponent.h"
+#include "HealthComponent.h"
 
 
 AMainCharacter::AMainCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	
 	spring_arm_comp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
 	spring_arm_comp->SetupAttachment(GetRootComponent());
-
+	
 	camera_comp = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
 	camera_comp->SetupAttachment(spring_arm_comp);
 
+	stamina_component = CreateDefaultSubobject<UStaminaComponent>("StaminaComponent");
+	health_component = CreateDefaultSubobject<UHealthComponent>("HealthComponent");
 }
 
 void AMainCharacter::BeginPlay()
@@ -33,6 +40,13 @@ void AMainCharacter::BeginPlay()
 			subsystem->AddMappingContext(input_mapping_context, 0);
 		}
 	}
+
+	if (stamina_component && health_component)
+	{
+		stamina_component->on_stamina_emty.AddDynamic(this, &AMainCharacter::on_stamina_emty);
+		health_component->on_death.AddDynamic(this, &AMainCharacter::death_handle);
+	}
+	
 }
 
 void AMainCharacter::Tick(float DeltaTime)
@@ -47,8 +61,18 @@ void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 
 	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
 	{
-		EnhancedInputComponent->BindAction(move_action, ETriggerEvent::Triggered, this, &AMainCharacter::move);
+		if (move_action && look_action && sprint_action)
+		{
+			EnhancedInputComponent->BindAction(move_action, ETriggerEvent::Triggered, this, &AMainCharacter::move);
+			EnhancedInputComponent->BindAction(look_action, ETriggerEvent::Triggered, this, &AMainCharacter::look);
+
+			EnhancedInputComponent->BindAction(sprint_action, ETriggerEvent::Started, this, &AMainCharacter::start_sprint);
+			EnhancedInputComponent->BindAction(sprint_action, ETriggerEvent::Completed, this, &AMainCharacter::stop_sprint);
+			EnhancedInputComponent->BindAction(sprint_action, ETriggerEvent::Canceled, this, &AMainCharacter::stop_sprint);
+		}
+	
 	}
+
 }
 
 void AMainCharacter::move(const FInputActionValue& value)
@@ -57,7 +81,116 @@ void AMainCharacter::move(const FInputActionValue& value)
 
 	if (Controller != nullptr)
 	{
-		AddMovementInput(FVector::ForwardVector, movement_vector.X);
-		AddMovementInput(FVector::RightVector, movement_vector.Y);
+		current_forward_input = movement_vector.Y;
+		if (is_sprinting && current_forward_input <= 0)
+		{
+			stop_sprint();
+		}
+		if (!is_sprinting)
+		{
+			if (movement_vector.Y < 0.f)
+			{
+				GetCharacterMovement()->MaxWalkSpeed = backward_walk_speed;
+			}
+			else
+			{
+				GetCharacterMovement()->MaxWalkSpeed = forward_walk_speed;
+			}
+		}
+		
+		const FRotator rotation = GetActorRotation();
+		const FRotator yaw_rotation(0, rotation.Yaw, 0);
+		const FVector forward_direction = FRotationMatrix(yaw_rotation).GetUnitAxis(EAxis::X);
+		const FVector right_direction = FRotationMatrix(yaw_rotation).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(forward_direction, movement_vector.Y);
+		AddMovementInput(right_direction, movement_vector.X);
+
+		if (is_sprinting)
+		{
+			if (GetVelocity().SizeSquared() < 100.f)
+			{
+				stamina_component->set_is_running(false);
+			}
+			else
+			{
+				stamina_component->set_is_running(true);
+			}
+		}
+
+	}
+}
+
+void AMainCharacter::look(const FInputActionValue& value)
+{
+	const FVector2D look_vector = value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		AddControllerYawInput(look_vector.X);
+		AddControllerPitchInput(look_vector.Y);
+	}
+}
+
+void AMainCharacter::start_sprint()
+{
+	
+	if (stamina_component && stamina_component->can_running())
+	{
+		
+		if (GetCharacterMovement() && current_forward_input > 0.f)
+		{
+			is_sprinting = true;
+			GetCharacterMovement()->MaxWalkSpeed = running_speed;
+
+			stamina_component->set_is_running(true);
+		}
+	}
+	
+}
+
+void AMainCharacter::stop_sprint()
+{
+
+	if (stamina_component)
+	{
+		stamina_component->set_is_running(false);
+	}
+
+	if (GetCharacterMovement())
+	{
+		is_sprinting = false;
+		GetCharacterMovement()->MaxWalkSpeed = forward_walk_speed;
+	}
+}
+
+void AMainCharacter::on_stamina_emty()
+{
+	stop_sprint();
+}
+
+void AMainCharacter::death_handle()
+{
+	if (APlayerController* pc = Cast<APlayerController>(GetController()))
+	{
+		DisableInput(pc);
+	}
+
+	if (GetCapsuleComponent())
+	{
+		GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		GetCapsuleComponent()->SetCollisionResponseToAllChannels(ECR_Ignore);
+	}
+
+	if (GetMesh())
+	{
+		GetMesh()->SetCollisionProfileName(TEXT("Ragdoll"));
+		GetMesh()->SetSimulatePhysics(true);
+	}
+
+	if (GetCharacterMovement())
+	{
+		GetCharacterMovement()->DisableMovement();
+		GetCharacterMovement()->StopMovementImmediately();
 	}
 }
--- Source/EscapeAndExtraction/Public/MainCharacter.h
+++ Source/EscapeAndExtraction/Public/MainCharacter.h
@@ -12,6 +12,8 @@ class USpringArmComponent;
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
+class UStaminaComponent;
+class UHealthComponent;
 
 UCLASS()
 class ESCAPEANDEXTRACTION_API AMainCharacter : public ACharacter
@@ -31,11 +33,35 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
 	UInputAction* move_action;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* look_action;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* sprint_action;
+
 
 protected:
 	
 	virtual void BeginPlay() override;
 	void move(const FInputActionValue& value);
+	void look(const FInputActionValue& value);
+	void start_sprint();
+	void stop_sprint();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float forward_walk_speed = 300.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float backward_walk_speed = 150.f;
+
+	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category = "Movement")
+	float running_speed = 600.f;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool is_sprinting = false;
+
+	UFUNCTION()
+	void on_stamina_emty();
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
 	USpringArmComponent* spring_arm_comp;
@@ -43,5 +69,18 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
 	UCameraComponent* camera_comp;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UStaminaComponent* stamina_component;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UHealthComponent* health_component;
+
+
+private:
+	float current_forward_input = 0;
+
+	UFUNCTION()
+	void death_handle();
+	
 
 };
