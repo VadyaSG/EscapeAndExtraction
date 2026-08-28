ae8117c68ef3c44552f6bd681864589df6e5c20f
VadyaS <notfoundmyname@gmail.com>
2026-08-28 17:53:37 +0300

    LevelDesignStarted

--- Source/EscapeAndExtraction/Private/GameSettings.cpp
+++ Source/EscapeAndExtraction/Private/GameSettings.cpp
@@ -7,14 +7,47 @@ UGameSettings::UGameSettings()
 {
 	mouse_sensivity = 1.f;
 	spawn_delay = 5.f;
-	key_card_ttl = 60.f;
+	key_card_ttl = 30.f;
 	enemy_modification = 1.f;
 	stamina_drain_rate = 10.f;
 	stamina_regen_rate = 8.f;
+	min_spawn_delay = 0.5f;
+	difficulty_step = 0.2f;
+	update_difficulty_time = 10.f;
 }
 
 float UGameSettings::GetEnemyModificationFactor()
 {
 	const UGameSettings* Settings = GetDefault<UGameSettings>();
 	return Settings ? Settings->enemy_modification : 1.0f;
+}
+
+float UGameSettings::get_key_kard_ttl()
+{
+	const UGameSettings* Settings = GetDefault<UGameSettings>();
+	return Settings ? Settings->key_card_ttl : 30.0f;
+}
+
+float UGameSettings::get_spawn_delay()
+{
+	const UGameSettings* Settings = GetDefault<UGameSettings>();
+	return Settings ? Settings->spawn_delay : 5.f;
+}
+
+float UGameSettings::get_min_spawn_delay()
+{
+	const UGameSettings* Settings = GetDefault<UGameSettings>();
+	return Settings ? Settings->min_spawn_delay : 0.5f;
+}
+
+float UGameSettings::get_dificulty_step()
+{
+	const UGameSettings* Settings = GetDefault<UGameSettings>();
+	return Settings ? Settings->difficulty_step : 0.2f;
+}
+
+float UGameSettings::get_update_difficulty_time()
+{
+	const UGameSettings* Settings = GetDefault<UGameSettings>();
+	return Settings ? Settings->update_difficulty_time : 10.f;
 }
--- Source/EscapeAndExtraction/Public/GameSettings.h
+++ Source/EscapeAndExtraction/Public/GameSettings.h
@@ -35,9 +35,32 @@ public:
 	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, Category = "Gameplay|Balancing")
 	float stamina_regen_rate;
 
+	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, Category = "Gameplay|Balancing")
+	float min_spawn_delay;
+
+	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, Category = "Gameplay|Balancing")
+	float difficulty_step;
+
+	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, Category = "Gameplay|Balancing")
+	float update_difficulty_time;
+
 	UFUNCTION(BlueprintPure, Category = "Gameplay", meta = (DisplayName = "Get Enemy Modification"))
 	static float GetEnemyModificationFactor();
 
+	UFUNCTION(BlueprintPure, Category = "Gameplay", meta = (DisplayName = "Get TTLCard Modification"))
+	static float get_key_kard_ttl();
+
+	UFUNCTION(BlueprintPure, Category = "Timings", meta = (DisplayName = "Get Spawn Delay"))
+	static float get_spawn_delay();
+
+	UFUNCTION(BlueprintPure, Category = "Gameplay", meta = (DisplayName = "Get min spawn delay"))
+	static float get_min_spawn_delay();
+
+	UFUNCTION(BlueprintPure, Category = "Gameplay", meta = (DisplayName = "Get difficulty step"))
+	static float get_dificulty_step();
+
+	UFUNCTION(BlueprintPure, Category = "Gameplay", meta = (DisplayName = "Get update difficulty time"))
+	static float get_update_difficulty_time();
 }; 
 
 
